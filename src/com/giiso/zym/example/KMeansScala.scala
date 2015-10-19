9e4d8d1649fddd87ea2d1df23e1311ac5d26aba9
Yingminzhou <csuyingminzhou@gmail.com>
2015-10-19 16:35:25 +0800

    Change-Id: I574cace546463a962698efd272ab35fab715cf5a

---
+++ src/com/giiso/zym/example/KMeansScala.scala
@@ -0,0 +1,72 @@
+package com.giiso.zym.example
+
+import org.apache.spark.SparkContext
+import org.apache.spark._
+
+object KMeansScala {
+  
+  def main(args:Array[String]){
+    if(args.length < 2){
+      println("Please input <dataFile> <knumbers>")
+      return
+    }
+    
+    //val master = args(0)
+    val dataFile = args(0)
+    val knumbers = args(1).toInt
+    
+    val conf = new SparkConf()    
+    val sc = new SparkContext(conf)
+    val lines = sc.textFile(dataFile)
+    
+    val points = lines.map ( line => {
+        val parts = line.split("\t").map(_.toDouble)
+          new Point(parts(0) , parts(1))
+    } ).cache()
+    
+    //初始化K个质心
+    val centroids = Array.fill(knumbers) {Point.random}
+    println("test points: ")
+    points.foreach(println(_))
+    println("initialuize centroids: \n" + centroids.mkString("\n") + "\n")
+    
+    val startTime = System.currentTimeMillis()
+    
+    val resultCentroids = kmeans(points,centroids,0.01,sc)
+    
+    val endTime = System.currentTimeMillis()
+    val runTime = endTime - startTime
+    println("run Time: " + runTime + "\nFinal centroids: \n"+ resultCentroids.mkString("\n"))
+    
+  }
+  
+  
+  def kmeans(points: rdd.RDD[Point], centroids: Seq[Point], epsilon: Double, sc: SparkContext): Seq[Point]={
+    def closestCentroid(point: Point) = {
+      centroids.reduceLeft((a,b) => if ((point distance a) < (point distance b)) a else b)
+    }
+    var cluster = points.map ( point => (closestCentroid(point) -> (point,1)) )
+    // cluster.foreach(println(_))
+    
+    var clusterSum = cluster.reduceByKey{ case ((ptA, numA),(ptB, numB)) => (ptA + ptB, numA + numB)}
+    var average = clusterSum.map {pair => (pair._1,pair._2._1/pair._2._2)}.collectAsMap()
+    
+    val newCentroids = centroids.map(oldCentroid => {
+      average.get(oldCentroid) match{
+        case Some(newCentroid) => newCentroid
+        case None => oldCentroid
+      }
+    })
+    
+    val movement = (centroids, newCentroids).zipped.map(_ distance _)
+    println("Centroids changed by \n" + movement.map(d => "%sf".format(d)).mkString("(",",",")") 
+    + "\nto\n" + newCentroids.mkString(",") + "\n")
+    
+    if (movement.exists(_>epsilon))
+      kmeans(points,newCentroids,epsilon,sc)
+    else 
+      newCentroids
+    
+  }
+  
+}
---
+++ src/com/giiso/zym/example/Point.scala
@@ -0,0 +1,17 @@
+package com.giiso.zym.example
+
+object Point {
+  def random() = {
+    new Point(math.random*50,math.random*50)
+  }
+}
+
+case class Point(val x: Double, val y:Double){
+  def +(that: Point) = new Point(this.x + that.x, this.y + that.y)
+  def -(that: Point) = new Point(this.x - that.x, this.y - that.y)
+  def /(d: Double) = new Point(this.x / d, this.y / d)
+  def pointLength = math.sqrt(x * x + y * y)
+  def distance(that: Point) = (this - that).pointLength
+  
+  override def toString = format("(%.3f,%.3f)",x,y)
+}
